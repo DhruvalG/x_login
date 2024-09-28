ffb108d4fae220758f8b4e4a392d5e032c82b424
DhruvalG <dhruval.gv@gmail.com>
2024-09-28 21:46:08 +0530

    Fix 6

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -32,8 +32,8 @@ const Login = () => {
         </div>
         {!auth && (
           <form onSubmit={handleSubmit}>
-            <label>username:<input label="Username" value={name} type="text" onChange={(e) => setName(e.target.value)} required  /></label>
-            <label>password:<input label="Username" value={pass} onChange={(e) => setPass(e.target.value)} type="password" required /></label>
+            <label>Username:<input label="Username" value={name} type="text" onChange={(e) => setName(e.target.value)} required  /></label>
+            <label>Password:<input label="Username" value={pass} onChange={(e) => setPass(e.target.value)} type="password" required /></label>
             <Button
               type="submit"
               variant="contained"
